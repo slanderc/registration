e65f371d219dec019e20548e074534f14b8b868f
slanderc <Vilyge1231@gmail.con>
2017-11-10 11:10:47 +0300

    Add watcher

--- gulpfile.js
+++ gulpfile.js
@@ -91,3 +91,25 @@ gulp.task('move:bootstrap', function(){
   gulp.src(path.src.bootstrap)
   .pipe(gulp.dest(path.build.css))
 });
+
+gulp.task('watch', function(){
+  watch([path.watch.html], function(event, cb) {
+    gulp.start('html:build');
+  });
+  watch([path.watch.style], function(event, cb) {
+    gulp.start('style:build');
+  });
+  watch([path.watch.js], function(event, cb) {
+    gulp.start('js:build');
+  });
+  watch([path.watch.img], function(event, cb) {
+    gulp.start('image:build');
+  });
+  watch([path.watch.fonts], function(event, cb) {
+    gulp.start('fonts:build');
+  });
+});
+
+gulp.task('webserver', function () {
+  browserSync(config);
+});
